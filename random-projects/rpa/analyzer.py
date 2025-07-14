a8b09b93a31291562507c7c19e5e8d2820b0fff3
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-07-14 16:57:01 -0400

    feat: added mean, median, min, max and sd annually, quarterly and globally

--- random-projects/rpa/analyzer.py
+++ random-projects/rpa/analyzer.py
@@ -14,6 +14,78 @@ from constants import (
 # Load the Excel file
 data = pd.read_excel(SALES_XLSX, engine="openpyxl")
 
+# Rename the columns for better readability
+columns_renamed = {
+    'Costo Vehículo': 'Costo de Vehículo',
+    'Precio Venta sin IGV': 'Precio Venta de Vehículo sin IGV',
+    'Precio Venta Real': 'Precio Venta de Vehículo con IGV',
+    'Ganancia': 'Ganancia por Venta',
+}
+data.rename(columns=columns_renamed, inplace=True)
+
+# Generate a profit column
+data['Ganancia por Venta'] = data['Precio Venta de Vehículo sin IGV'] - data[(
+    'Costo de Vehículo')]
+
+# Define the numerical columns to analyze and their statistics functions
+numerical_columns = [
+    'Costo de Vehículo',
+    'Precio Venta de Vehículo sin IGV',
+    'Precio Venta de Vehículo con IGV',
+    'Ganancia por Venta',
+]
+stats_fns = ['min', 'max', 'mean', 'median', 'std']
+stats_fns_renamed = {
+    'min': 'Mínimo',
+    'max': 'Máximo',
+    'mean': 'Media',
+    'median': 'Mediana',
+    'std': 'Desviación Estándar'
+}
+numerical_data = data[numerical_columns]
+
+# Get the min, max, mean, median, and standard deviation for the numerical columns
+numerical_stats = numerical_data.agg(stats_fns)
+numerical_stats.rename(index=stats_fns_renamed, inplace=True)
+
+# Define the numerical columns to analyze plus the 'Fecha' column
+numerical_with_dt_columns = ['Fecha',*numerical_columns]
+numerical_with_dt_data = data[numerical_with_dt_columns]
+
+# Generate an anual numerical summary
+anual_summary = numerical_with_dt_data.groupby(data['Fecha'].dt.year).agg({
+    'Costo de Vehículo': stats_fns,
+    'Precio Venta de Vehículo sin IGV': stats_fns,
+    'Precio Venta de Vehículo con IGV': stats_fns,
+    'Ganancia por Venta': stats_fns
+})
+anual_summary.columns = [
+    f"{col} Anual ({stats_fns_renamed[stat]})" for col, stat in anual_summary.columns
+]
+#anual_summary.rename(
+#    index=stats_fns_renamed,
+#    columns = [f"{col} Anual ({stat})" for col, stat in anual_summary.columns]
+#)
+
+# Generate a quarterly numerical summary
+quarterly_summary = numerical_with_dt_data.groupby(data['Fecha'].dt.to_period('Q')).agg({
+    'Costo de Vehículo': stats_fns,
+    'Precio Venta de Vehículo sin IGV': stats_fns,
+    'Precio Venta de Vehículo con IGV': stats_fns,
+    'Ganancia por Venta': stats_fns
+})
+quarterly_summary.columns = [
+    f"{col} Trimestral ({stats_fns_renamed[stat]})" for col, stat in quarterly_summary.columns
+]
+#anual_summary.rename(
+#    index=stats_fns_renamed,
+#    columns = [f"{col} Trimestral ({stat})" for col, stat in
+#               quarterly_summary.columns]
+#)
+
+# Remove hour from 'Fecha' column
+data['Fecha'] = data['Fecha'].dt.date
+
 # Count the number of appearances of each data in the 'Segmento' column
 segment_counts = data['Segmento'].value_counts()
 
@@ -29,9 +101,6 @@ top_counts = {}
 for column, counts in columns_counts.items():
     top_counts[column] = counts.head(5)
 
-# Remove hour from 'Fecha' column
-data['Fecha'] = data['Fecha'].dt.date
-
 def generate_summary():
     """
     Function to generate a summary of the sales data.
@@ -46,7 +115,10 @@ def generate_summary():
         "Top 5 Sedes con mayor cantidad de ventas": top_counts[
             'Sede'].to_dict(),
         "Top 5 Vendedores con más ventas realizadas": top_counts[
-            'Vendedor'].to_dict()
+            'Vendedor'].to_dict(),
+        "Estadísticas numéricas": numerical_stats.to_dict(),
+        "Resumen anual": anual_summary.to_dict(),
+        "Resumen trimestral": quarterly_summary.to_dict(),
     }
 
 def generate_pdf(
